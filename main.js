b4986af550d9c7e3514faa90ba1a504255227b8b
tanishs26 <tanishsharma2627@gmail.com>
2024-05-19 10:12:40 +0530

    Changes in js and css

--- main.js
+++ main.js
@@ -1,80 +1,76 @@
-const add = document.querySelector("#addNew");
-const pop = document.querySelector(".popup");
+const addNew = document.querySelector("#addNew");
 const submit = document.querySelector("#submit");
+const popup = document.querySelector(".popup");
 const bookInfo = document.querySelector(".bookInfo");
-const names = document.querySelector("#name");
-const writer = document.querySelector("#writer");
-const pages = document.querySelector("#pages");
-const check = document.querySelector("#check");
+const bName = document.querySelector("#name");
+const bAuthor = document.querySelector("#writer");
+const bPage = document.querySelector("#pages");
+const bRead = document.querySelector("#check");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
-
-const meadows = new Book("Meadows", "Seth Roll", 259, "read");
+const meadows = new Book("Meadows", "T.Sharma", 269, "Read");
 const myLibrary = [meadows];
-
-const tanish = new Book("hell", "Seth", 23, "read");
-addBookToLibrary(tanish);
-
-function addBookToLibrary(book) {
+function addToLibrary(book) {
   myLibrary.push(book);
 }
-
-add.addEventListener("click", () => {
-  pop.style.display = "flex";
+addNew.addEventListener("click", () => {
+  popup.style.display = "flex";
 });
-
 submit.addEventListener("click", () => {
-  const readY = check.checked ? "read" : "not read";
-  let newBook = new Book(names.value, writer.value, pages.value, readY);
-  addBookToLibrary(newBook);
-  displayBooks();
-
-  pop.style.display = "none";
-  names.value = "";
-  writer.value = "";
-  pages.value = "";
-  readY = false;
+  const bReady = bRead.checked ? "Read" : "Not Read";
+  let newBook = new Book(bName.value, bAuthor.value, bPage.value, bReady);
+  addToLibrary(newBook);
+  displayBook();
+  popup.style.display = "none";
+  bName.value = "";
+  bAuthor.value = "";
+  bRead.checked = false;
+  bPage.value = "";
 });
 
-function displayBooks() {
-  bookInfo.innerHTML = "";
+function displayBook() {
+  bookInfo.innerHTML='';
   for (let i = 0; i < myLibrary.length; i++) {
     const bookDiv = document.createElement("div");
-    bookDiv.style.backgroundColor = "yellow";
+    bookDiv.classList.add('book-div')
 
-    const bookTitle = document.createElement("p");
+    const bookTitle = document.createElement("div");
     bookTitle.textContent = myLibrary[i].title;
 
-    const bookAuthor = document.createElement("p");
+    const bookAuthor = document.createElement("div");
     bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
 
-    const bookPages = document.createElement("p");
-    bookPages.textContent = `Pages:${myLibrary[i].pages}`;
+    const bookPage = document.createElement("div");
+    bookPage.textContent = `Page: ${myLibrary[i].pages}`;
 
     const bookRead = document.createElement("button");
-    bookRead.textContent = `${myLibrary[i].read}`;
-    bookRead.addEventListener('click',()=>{
-        myLibrary[i].read=myLibrary[i].read==='read'?'not read':'read'
-        displayBooks()
-    })
+    bookRead.classList.add('read-button')
+    bookRead.textContent=`${myLibrary[i].read}`
+    bookRead.addEventListener("click", () => {
+      myLibrary[i].read =  myLibrary[i].read == "Read" ? "Not Read" : "Read";
+      displayBook();
+    });
 
-    const bookDelete = document.createElement("button");
-    bookDelete.textContent = "Delete";
-    bookDelete.addEventListener("click", () => {
-      myLibrary.splice(i, 1);
-      displayBooks();
+    const bookDel = document.createElement("button");
+    bookDel.textContent = "Delete";
+    bookDel.classList.add("delete-button");
+
+    bookDel.addEventListener("click", () => {
+       myLibrary.splice(i,1);
+       displayBook();
     });
 
     bookDiv.appendChild(bookTitle);
     bookDiv.appendChild(bookAuthor);
-    bookDiv.appendChild(bookPages);
+    bookDiv.appendChild(bookPage);
     bookDiv.appendChild(bookRead);
-    bookDiv.appendChild(bookDelete);
+    bookDiv.appendChild(bookDel);
     bookInfo.appendChild(bookDiv);
   }
 }
-displayBooks();
+displayBook();
